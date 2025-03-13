644d29278fa741be5fd5d0093aafe77f0c46e00b
mn473@cornell.edu <mn473@cornell.edu>
2025-03-12 20:22:57 -0400

    Manage base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/mn473.github.io",
+  base: "/portfolio",
 })
